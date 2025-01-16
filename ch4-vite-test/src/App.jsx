6caf109cdf7166f9671e0c356aa7cdb409156bb1
Pkwangho <you@example.com>
2025-01-16 09:05:05 +0900

    7th commit_250116

---
+++ ch4-vite-test/src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+import EventPractice from './component/EventPractice'
+import EventPractice2 from './component/EventPractice2'
+import Say from './component/Say'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='react'>ch4 이벤트 핸들러 해보기</h1>
+      <Say />
+      <EventPractice />
+      <EventPractice2 />
+    </>
+  )
+}
+
+export default App
